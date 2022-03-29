7917321c11335046a2b6229114569437a12b6fb5
Joshua Peake <55125529@ad.mmu.ac.uk>
2022-03-29 16:53:55 +0100

    Change "doTours" to "doTourGen"
    New method performs a single iteration, to help with plotting. Local search also moved to this class, performed only on best tour rather than every tour

--- AntSystem.py
+++ AntSystem.py
@@ -2,6 +2,7 @@ import math
 import pandas as pd
 import streamlit as st
 import numpy as np
+import RenderManager as rm
 import time
 from Ant import Ant
 import sys
@@ -24,15 +25,23 @@ class AntSystem:
     rho = 0.98
     greedyLength = 0
     mmasConst = 0
+    pherMin = 0
+    markerSize = 0
+    data = []
+    markerColour = ""
+    lineColour = ""
 
-    def __init__ (self, data, nAnts, alpha, beta, rho, numNN):
+
+    def __init__ (self, data, nAnts, alpha, beta, rho, numNN, rm):
         self.reset()
         self.loadFile(data)
+        self.data = data
         self.nAnts = nAnts
         self.rho = rho
         self.numNN = numNN
         self.calcNN()
         self.greedyTour()
+        self.renderManager = rm
         self.pher = np.full((self.numVerts, self.numVerts), float(1/(self.greedyLength*(1-rho))))
         for i in range(nAnts):
             self.ants.append(Ant(self.edgeWeights,self.pher,self.numVerts, alpha, beta, self.nnList))
@@ -73,29 +82,27 @@ class AntSystem:
            
             self.nnList.append(sortedIndicies[1:self.numNN-1])
 
-    def doTours(self, nIter):
-        startTime = time.perf_counter()
-        for i in range(nIter):
-            for ant in self.ants:
-                ant.constructTour()
-                self.tours.append(ant.currTour)
-            self.getBestTour()
-            self.bestTour = self.tours[self.bestTourIndex]
-            if (i+1)%10 == 0:
-                print(str(i+1) + ": "+ str(self.bestTourDist))
-            self.updatePher()
-            self.pherDecay()
-            self.tours.clear()
-        endTime = time.perf_counter()
-        print(endTime-startTime)
+    def doTourGen(self):
+        for ant in self.ants:
+            ant.constructTour()
+            self.tours.append(ant.currTour)
+        self.getBestTour()
+        self.two_opt()
+        self.bestTourDist = self.getTourDist(self.bestTour)
+        # print("After LS 2: " +  str(self.bestTourDist))
+        self.updatePher()
+        self.pherDecay()
+        self.tours.clear()
+
         
 
     def getBestTour(self):
         for i, tour in enumerate(self.tours):
             dist = self.getTourDist(tour)
             if dist < self.bestTourDist:
+                self.bestTour = tour.copy()
                 self.bestTourDist = dist
-                self.bestTourIndex = i
+                #print("Original Original: " + str(dist))
 
 
     def getTourDist(self, tour):
@@ -111,12 +118,12 @@ class AntSystem:
         tour = self.bestTour
         newPher = 1/self.bestTourDist
         pherMax = 1/((1-self.rho) * self.bestTourDist)
-        pherMin = pherMax * self.mmasConst
+        self.pherMin = pherMax * self.mmasConst
         for i in range (len(tour)-1):
             currVert = tour[i]
             nextVert = tour[i+1]
             self.pher[currVert][nextVert] += newPher
-        self.pher = np.clip(self.pher, pherMin, pherMax)
+        self.pher = np.clip(self.pher, self.pherMin, pherMax)
 
     def pherDecay(self):
         self.pher = np.dot(self.pher, self.rho)
@@ -126,6 +133,23 @@ class AntSystem:
         for i in range(self.numVerts):
             gTour.append(i)
         self.greedyLength = self.getTourDist(gTour)
+
+    def two_opt(self):
+        #print("Orig 2: " + str(self.getTourDist(self.bestTour)))
+        best = self.bestTour
+        improved = True
+        while improved:
+            improved = False
+            for i in range(1, len(self.bestTour) - 2):
+                for j in range(i + 1, len(self.bestTour)):
+                    if j - i == 1: continue
+                    if self.cost_change(best[i - 1], best[i], best[j - 1], best[j]) < 0:
+                        best[i:j] = best[j - 1:i - 1:-1]
+                        improved = True
+
+    def cost_change(self, n1, n2, n3, n4):
+        return self.edgeWeights[n1][n3] + self.edgeWeights[n2][n4] - self.edgeWeights[n1][n2] - self.edgeWeights[n3][n4]
+
 
 
         
