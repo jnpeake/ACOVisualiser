14f384f840846c020152785585741329bb75d89f
Joshua Peake <55125529@ad.mmu.ac.uk>
2022-03-29 16:52:30 +0100

    Add updating TSP/Pheromone plots
    TSP plot and new Pheromone plot update as the algorithm runs, showing the development of both the tour and the pheromone matrix

--- ACOVisualiser.py
+++ ACOVisualiser.py
@@ -1,14 +1,15 @@
+from logging import PlaceHolder
 import streamlit as st
 import pandas as pd
 import numpy as np
-import VisualiseTSP as vTSP
-import VisualiseTour as vTour
+from RenderManager import RenderManager as rm
 import Utility as ut
 from AntSystem import AntSystem
 
 st.title("Ant Colony Optimisation")
 
 marker_colour = st.sidebar.selectbox("Marker Colour", ("red", "black", "blue", "green"), index = 0)
+line_colour = st.sidebar.selectbox("Line Colour", ("red", "black", "blue", "green"), index = 0)
 marker_size = st.sidebar.selectbox("Marker Size", ("0.25","0.5","1", "2", "3", "4"), index = 3)
 nAnts = st.sidebar.number_input("Number of Ants", min_value = 1, max_value = 100)
 nIter = st.sidebar.number_input("Number of Iterations", min_value = 1, max_value = 10000)
@@ -18,24 +19,38 @@ rho = st.sidebar.number_input("Rho", format="%.2f", min_value = 0.01, max_value 
 numNN = st.sidebar.number_input("Nearest Neighbours", min_value = 4, max_value = 32, value = 16)
 
 option = st.selectbox('Select TSP', (ut.getFileNames()))
-data_raw = vTSP.open_tsp(option)
-data = vTSP.convert_data(data_raw)
+data_raw = ut.open_tsp(option)
+data = ut.convert_data(data_raw)
 if st.checkbox("Show raw data"):
     st.subheader("Raw Data")
     st.write(data)
 
-col1, col2 = st.columns(2)
+renderManager = rm(marker_colour, line_colour, marker_size)
 
-with col1:
-    st.subheader("TSP")
-    with st.spinner("Rendering TSP..."):
-        st.pyplot(vTSP.renderPlot(data, marker_colour, marker_size))
-with col2:
-    antSys = AntSystem(data, nAnts, alpha, beta, rho, numNN)
-    st.subheader("Tour")
-    with st.spinner("Creating tour"):
-        antSys.doTours(nIter)
-        st.pyplot(vTour.renderTour(data, antSys.bestTour, marker_colour, marker_size))
+
+st.subheader("TSP")
+with st.spinner("Rendering TSP..."):
+    st.pyplot(renderManager.renderPlot(data))
+
+antSys = AntSystem(data, nAnts, alpha, beta, rho, numNN, renderManager)
+
+prog_bar = st.progress(0)
+placeholder = st.empty()
+
+for i in range(nIter):
+    antSys.doTourGen()
+    if nIter > 1:
+        prog_bar.progress(i/(nIter-1))
+    if i % 10 == 0:
+        with placeholder.container():
+            col1, col2= st.columns(2)
+            with col1:
+                tourPlaceholder = st.pyplot(renderManager.renderTour(data, antSys.bestTour))
+            with col2:
+                pherPlaceholder = st.pyplot(renderManager.renderPher(data, antSys.pher))
+            print(str(i) + ": "+ str(antSys.bestTourDist))
+    
+    
 
 st.subheader("Best tour distance: "+ str(antSys.bestTourDist))
 
