1a955206408b3f78233a0e45d295c5fc98aceb57
Joshua Peake <55125529@ad.mmu.ac.uk>
2022-03-29 16:54:40 +0100

    Add non-plotting methods from visualise classes

--- Utility.py
+++ Utility.py
@@ -1,5 +1,6 @@
 import glob
 import os
+import pandas as pd
 
 def getFileNames():
     tspFiles = []
@@ -7,3 +8,24 @@ def getFileNames():
         tspFiles.append(os.path.basename(file))
     tspFiles.sort()
     return tspFiles
+
+def open_tsp (filename):
+    tsp_file = open("Instances/" + filename, 'r')
+    data_raw = tsp_file.readlines()
+    tsp_file.close()
+    data = []
+    for line in data_raw:
+        if line != "\n": 
+            if line.strip()[0].isdigit():
+                data.append(line)
+    return data
+
+def convert_data(data):
+    df = pd.DataFrame(data, columns = ["val"])
+    df2 = df["val"].str.split(n = 2, expand = True)
+    df["Index"] = df2[0]
+    df["X"] = df2[1]
+    df["Y"] = df2[2]
+    df.drop(columns = "val", inplace = True)
+    df[["X", "Y"]] = df[["X", "Y"]].astype(float)
+    return df
